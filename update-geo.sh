398b7886ea9243493995b1b145e76112aba046f9
dhcn2000 <112333167+dhcn2000@users.noreply.github.com>
2025-02-03 20:49:03 +0800

    Update update-geo.sh

--- update-geo.sh
+++ update-geo.sh
@@ -6,8 +6,8 @@ echo "更新资源文件………"
 echo "~~~~~~~~~~~~~~"
 cd /opt/mosdns/bin || exit
 
-wget --show-progress -t 5 -T 10 -cqO accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
-wget --show-progress -t 5 -T 10 -cqO apple.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/apple.china.conf
+wget --show-progress -t 5 -T 10 -cqO accelerated-domains.china.conf https://gh-proxy.com/https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+wget --show-progress -t 5 -T 10 -cqO apple.china.conf https://gh-proxy.com/https://github.com/felixonmars/dnsmasq-china-list/raw/master/apple.china.conf
 sed -r 's/.{8}//' accelerated-domains.china.conf > accelerated-domains.china.conf2
 sed -r 's/.{16}$//' accelerated-domains.china.conf2 > accelerated-domains.china.conf.raw.txt
 sed -r 's/.{8}//' apple.china.conf > apple.china.conf2
